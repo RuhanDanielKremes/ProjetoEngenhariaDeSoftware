6a437288d65b50d373ccc7c25adddd31bf5b2d3b
Igor-Maceno <igorlopesmaceno17@gmail.com>
2024-04-25 22:47:06 -0300

    Routes config

--- geometry-kids/src/App.js
+++ geometry-kids/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+//Pages
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Geometry Kids</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
